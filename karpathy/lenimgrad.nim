51b4f3d2f28c7bb0962f6194407ccbfdba391646
Pietro Peterlongo <pietro.peterlongo@gmail.com>
2023-11-04 00:26:54 +0100

    pause watching video

--- karpathy/lenimgrad.nim
+++ karpathy/lenimgrad.nim
@@ -1,19 +1,28 @@
-type Value* = object
-  data*: float
+type
+  ValueKind = enum Atom, Op
+  OpKind = enum Plus = "+", Times = "*" 
+  Value* = ref object
+    data*: float
+    case kind: ValueKind
+    of Atom:
+      discard
+    of Op:
+      op: OpKind
+      left: Value
+      right: Value
 
 func `+`*(a, b: Value): Value =
-  Value(data: a.data + b.data)
-
+  Value(data: a.data + b.data, kind: Op, op: Plus, left: a, right: b)
 
 func `*`*(a, b: Value): Value =
-  Value(data: a.data * b.data)
+  Value(data: a.data * b.data, kind: Op, op: Times, left: a, right: b)
 
 when isMainModule:
   import print # goody from treeform, should be in stdlib
 
   let a = Value(data: 2.0)
 
-  echo a #-> (data: 2.0)
+  # echo a #-> (data: 2.0)
   print a #-> a=Value(data: 2.0)
 
   let b = Value(data: -3.0)
@@ -24,3 +33,31 @@ when isMainModule:
   print (let d = (a*b) + c; d) # walrus like semantic for free
   #-> let d = (a * b) + c
   #-> d=Value(data: 4.0)
+
+  #[after refactoring:
+
+  a=Value:ObjectType(data: 2.0, kind: "Atom")
+  a + b=Value:ObjectType(
+    data: -1.0,
+    kind: "Op",
+    op: "+",
+    left: Value:ObjectType(data: 2.0, kind: "Atom"),
+    right: Value:ObjectType(data: -3.0, kind: "Atom")
+  )
+
+  let d = (a * b) + c
+  d=Value:ObjectType(
+    data: 4.0,
+    kind: "Op",
+    op: "+",
+    left: Value:ObjectType(
+      data: -6.0,
+      kind: "Op",
+      op: "*",
+      left: Value:ObjectType(data: 2.0, kind: "Atom"),
+      right: Value:ObjectType(data: -3.0, kind: "Atom")
+    ),
+    right: Value:ObjectType(data: 10.0, kind: "Atom")
+  )
+
+  ]#
--- karpathy/readme.md
+++ karpathy/readme.md
@@ -20,7 +20,17 @@ lenimgrad
 - [x] multiply
   - vs python: showing a walrus like operator we get for free
 - vs python: up to now the most odd thing we have notation wise is oberon's `*` operator for exporting (instead of pub keyboard or everything public and underscore means private convention as in python)
+- [x] children and op
+  - vs python: need to refactor Value in ref object
+  - I like the enums Op and ValueKind
+  - print does a great job at visualizing this things
+- graph, skipping for now
+  - [ ] I could use mermaid in nimib
+- label, skipping for now
+  - [ ] could use metaprogramming to avoid needing to specify label
+- PAUSE: stopped watching after explaining manual grad (and before the neuron)
+https://youtu.be/VMj-3S1tku0?si=kmf-l2pZsRmplNvw&t=3182
+
 
 inspiration: I should have a `nb` command from nimib that when it is run against a nim file it decorates it with outputs
 (if I add also a shortcut from editor it would make a pretty nice DX)
-
