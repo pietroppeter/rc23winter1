158c9acfc9db3519890a63218c3ff2ddd5d3e889
Pietro Peterlongo <pietro.peterlongo@gmail.com>
2024-02-01 16:46:55 +0100

    proceso examples

---
+++ pyscript/proceso_readme_static_sketch/sketch.py
@@ -0,0 +1,28 @@
+# from proceso readme
+from proceso import Sketch
+
+
+p5 = Sketch()
+p5.describe("A rectangle, circle, triangle, and flower drawn in pink on a gray background.")
+
+# Create the canvas
+p5.create_canvas(720, 400)
+p5.background(200)
+
+# Set colors
+p5.fill(204, 101, 192, 127)
+p5.stroke(127, 63, 120)
+
+# A rectangle
+p5.rect(40, 120, 120, 40)
+# A circle
+p5.circle(240, 240, 80)
+# A triangle
+p5.triangle(300, 100, 320, 100, 310, 80)
+
+# A design for a simple flower
+p5.translate(580, 200)
+p5.no_stroke()
+for _ in range(10):
+    p5.ellipse(0, 30, 20, 80)
+    p5.rotate(p5.PI / 5)
